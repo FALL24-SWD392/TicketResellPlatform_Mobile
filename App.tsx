fcdcbedd82cef3f9633420a4b3e67dcd6108f1c5
neru <ngocngocthuc@gmail.com>
2024-09-16 12:12:56 +0700

    fix(build): tailwind theme config

--- App.tsx
+++ App.tsx
@@ -3,6 +3,7 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { Home } from "./src/pages/home";
 import { Profile } from "./src/pages/profile";
+import Ionicons from '@expo/vector-icons/Ionicons';
 
 function App() {
 
@@ -10,7 +11,26 @@ function App() {
 
   return (
     <NavigationContainer>
-      <Tab.Navigator>
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
+
+            if (route.name === 'Home') {
+              iconName = focused
+                ? 'home'
+                : 'home-outline';
+            } else if (route.name === 'Profile') {
+              iconName = focused ? 'list-circle' : 'list-circle-outline';
+            }
+
+            // You can return any component that you like here!
+            return <Ionicons name={iconName} size={size} color={color} />;
+          },
+          tabBarActiveTintColor: 'tomato',
+          tabBarInactiveTintColor: 'gray',
+        })}
+      >
         <Tab.Screen name="Home" component={Home} />
         <Tab.Screen name="Profile" component={Profile} />
       </Tab.Navigator>
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,15 +6,66 @@ module.exports = {
   },
   theme: {
     colors: {
-      'blue': '#1fb6ff',
-      'purple': '#7e5bef',
-      'pink': '#ff49db',
-      'orange': '#ff7849',
-      'green': '#13ce66',
-      'yellow': '#ffc82c',
-      'gray-dark': '#273444',
-      'gray': '#8492a6',
-      'gray-light': '#d3dce6',
+      white: {
+        light: '#fefffe', // Light
+        lightHover: '#fefffe', // Light :hover
+        lightActive: '#fdffff', // Light :active
+        normal: '#f7f7f7', // Normal
+        normalHover: '#dee6de', // Normal :hover
+        normalActive: '#c6cccc', // Normal :active
+        dark: '#b9bfb9', // Dark
+        darkHover: '#949994', // Dark :hover
+        darkActive: '#6f736f', // Dark :active
+        darker: '#565956', // Darker
+      },
+      black: {
+        light: '#e8eeef', // Light
+        lightHover: '#dde5e7', // Light :hover
+        lightActive: '#b8cacc', // Light :active
+        normal: '#1a535c', // Normal
+        normalHover: '#174b53', // Normal :hover
+        normalActive: '#15424a', // Normal :active
+        dark: '#143e45', // Dark
+        darkHover: '#103237', // Dark :hover
+        darkActive: '#0c2529', // Dark :active
+        darker: '#091d20', // Darker
+      },
+      green: {
+        light: '#edfaf9', // Light
+        lightHover: '#e4f8f6', // Light :hover
+        lightActive: '#c8f0ed', // Light :active
+        normal: '#4eccd4', // Normal
+        normalHover: '#46b9b0', // Normal :hover
+        normalActive: '#3ea49d', // Normal :active
+        dark: '#3b9a93', // Dark
+        darkHover: '#2f7b76', // Dark :hover
+        darkActive: '#235c58', // Dark :active
+        darker: '#1b4845', // Darker
+      },
+      red: {
+        light: '#FFF0F0', // Light
+        lightHover: '#FFE9E9', // Light :hover
+        lightActive: '#FFD1D1', // Light :active
+        normal: '#FF6B6B', // Normal
+        normalHover: '#E66060', // Normal :hover
+        normalActive: '#CC5656', // Normal :active
+        dark: '#BF5050', // Dark
+        darkHover: '#994040', // Dark :hover
+        darkActive: '#733030', // Dark :active
+        darker: '#592525', // Darker
+      },
+      yellow: {
+        light: '#FFFDF0', // Light
+        lightHover: '#FFFBE9', // Light :hover
+        lightActive: '#FFF7D2', // Light :active
+        normal: '#FFE66D', // Normal
+        normalHover: '#E6CF62', // Normal :hover
+        normalActive: '#CCB857', // Normal :active
+        dark: '#BFAD52', // Dark
+        darkHover: '#998A41', // Dark :hover
+        darkActive: '#736731', // Dark :active
+        darker: '#595126', // Darker
+      },
     },
     fontFamily: {
       sans: ['Graphik', 'sans-serif'],
